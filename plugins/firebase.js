935617206cf0da1b26ca4997b63e7ab96637f46f
kmmhrr399 <70920912+kmmhrr399@users.noreply.github.com>
2022-09-15 23:26:29 +0900

    2022091501 firebase hosting OK

---
+++ plugins/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDj0iJjFlV-NbF0LZKJcEMBAKVzA-JF6ok",
+    authDomain: "vuemermaidmarkdown1.firebaseapp.com",
+    projectId: "vuemermaidmarkdown1",
+    storageBucket: "vuemermaidmarkdown1.appspot.com",
+    messagingSenderId: "843442527675",
+    appId: "1:843442527675:web:6e75820d1903377939f89e",
+    measurementId: "G-0QHG8DC1N2"
+}
+
+const firebase = initializeApp(firebaseConfig)
+const database = getFirestore(firebase)
+const analytics = getAnalytics(firebase)
+
+export default database
